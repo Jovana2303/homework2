00a23f24a4a5a04384778fb7c05364519a861e72
User <User@DESKTOP-DL1QKFV>
2024-03-06 20:23:30 +0100

    Add project files.

---
+++ task01/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numbers = new int[6];
+int sum = 0;
+
+for (int i = 0; i < 6; i++)
+{
+    Console.Write("Enter " + (i + 1) + " integer: ");
+    bool success = int.TryParse(Console.ReadLine(), out numbers[i]);
+    if (success)
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Wrong input");
+    }
+}
+for (int i = 0; i < 6; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        sum += numbers[i];
+    }
+}
+Console.WriteLine("The result is: " + sum);
---
+++ task02/Program.cs
@@ -0,0 +1,39 @@
+﻿string[] studentsG1 = new string[5];
+string[] studentsG2 = new string[5];
+
+for (int i = 0; i < 5; i++)
+{
+    int j;
+    Console.Write("Enter " + (j = i + 1) + " student from G1: ");
+    studentsG1[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Enter " + (i + 1) + " student from G2: ");
+    studentsG2[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Enter student group");
+int.TryParse(Console.ReadLine(), out int group);
+
+if (group == 1)
+{
+    Console.WriteLine("The Students in G1 are:");
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(studentsG1[i]);
+    }
+}
+else if (group == 2)
+{
+    Console.WriteLine("The Students in G2 are:");
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(studentsG2[i]);
+    }
+}
+else
+{
+    Console.WriteLine("Wrong input");
+}
